8d16a3ad686b2dca34c926ed8a6ddd45dabf46e1
abstract⁷ <lexusvp001@gmail.com>
2023-09-12 14:57:14 +0200

    Update initiate-send-email.js

--- license/js/initiate-send-email.js
+++ license/js/initiate-send-email.js
@@ -59,3 +59,14 @@ const DataModule = (function () {
     };
 
 })();
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Wait for the DOM to be fully loaded
+    const resultDiv = document.getElementById('resultDiv'); // Replace 'resultDiv' with the actual ID of your target div
+
+    // Call the appropriate function to get the data from DataModule
+    const fetchedData = DataModule.getFetchedData();
+
+    // Set the innerHTML of 'resultDiv' to the fetched data
+    resultDiv.innerHTML = fetchedData;
+});
