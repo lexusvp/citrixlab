45d2ebabcba071efb7d88f66c8e913859b7d25f1
abstract⁷ <144500546+CitrixLab@users.noreply.github.com>
2023-09-15 10:10:08 +0800

    Update my script.js

--- license/node/my script.js
+++ license/node/my script.js
@@ -5,27 +5,27 @@ const fs = require('fs');
 const path = require('path');
 
 // Specify the folder and file details
-const folderPath = 'file'; // Replace with your desired folder path
+const folderPath = 'myFolder'; // Replace with your desired folder path
 const fileName = 'example.txt';
 const filePath = path.join(folderPath, fileName);
 const fileContent = 'This is the content of the file.';
 
-// Check if the folder exists
+// Check if the folder exists, and create it if it doesn't
 if (!fs.existsSync(folderPath)) {
-  console.log(`The folder "${folderPath}" does not exist.`);
-  // You can choose to create the folder here if needed using fs.mkdirSync(folderPath)
+  fs.mkdirSync(folderPath, { recursive: true }); // Create the folder and any missing parent folders
+  console.log(`The folder "${folderPath}" was created.`);
+}
+
+// Check if the file already exists
+if (fs.existsSync(filePath)) {
+  console.log(`The file "${fileName}" already exists.`);
 } else {
-  // Check if the file already exists
-  if (fs.existsSync(filePath)) {
-    console.log(`The file "${fileName}" already exists.`);
-  } else {
-    // Create the file
-    fs.writeFile(filePath, fileContent, (err) => {
-      if (err) {
-        console.error('Error creating the file:', err);
-      } else {
-        console.log('File created successfully.');
-      }
-    });
-  }
+  // Create the file
+  fs.writeFile(filePath, fileContent, (err) => {
+    if (err) {
+      console.error('Error creating the file:', err);
+    } else {
+      console.log('File created successfully.');
+    }
+  });
 }
